fac4d17106c61231321e9e326abae1386e496b5f
AarushiswinnerYo <156099106+AarushiswinnerYo@users.noreply.github.com>
2025-07-02 17:30:40 +0530

    WOKING HARD

--- client.py
+++ client.py
@@ -8,67 +8,45 @@ FORMAT='utf-8'
 DISCONNECT_MSG="!disconnect"
 loggedin=False
 client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
 
-def send(msg):
-    global loggedin
-    global s
-    message=msg.encode(FORMAT)
-    msg_length=len(message)
-    send_length=str(msg_length).encode(FORMAT)
-    send_length+=b' '*(HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
-    f=client.recv(8192).decode(FORMAT)
-    if f=="User signed up successfully":
-        print("Signed up!")
-        if os.path.isdir("userprofs"):
-            os.chdir("userprofs")
-            with open(f"{s}.prof", "w") as w:
-                w.write("")
+def main(msg):
+    def send(msg):
+        global loggedin
+        global s
+        if msg=="!connect":
+            client.connect(ADDR)
+            return
+        elif msg==DISCONNECT_MSG:
+            client.close()
         else:
-            os.mkdir("userprofs")
-            os.chdir("userprofs")
-            with open(f"{s}.prof", "w") as w:
-                w.write("")
-        send(f"user-{s}")
-    elif f=="Successfully logged in!":
-        loggedin=True
-        s=s[0]
-        send(f"user-{s}")
-    elif f=="Non-existent":
-        quit()
-    elif f=="Incorrect password":
-        quit()
-    else:
-        print(f)
-
-if os.path.isdir("./userprofs"):
-    global s
-    profiles=[]
-    for r in os.listdir("./userprofs"):
-        if r.endswith(".prof"):
-            profiles.append(r)
-        for i in profiles:
-            print(f"{profiles.index(i)+1}. {i}")
-        sno=int(input("Choose user:- "))
-        p=input("Enter Password:- ")
-        sf=profiles[sno-1]
-        s=sf.split(".")
-        send(f"login-{s[0]}-{p}")
-    else:
-        if not loggedin:
-            print("Fail 1")
-            s=input("Enter username to signup:- ")
-            p=input("Enter password:- ")
-            send(f"signup-{s}-{p}")
-else:
-    if not loggedin:
-        s=input("Enter username to signup:- ")
-        p=input("Enter password:- ")
-        send(f"signup-{s}-{p}")  
-while True:
-    i=input("Enter A text: ")
-    send(i)
-    if i==DISCONNECT_MSG:
-        break
+            message=msg.encode(FORMAT)
+            msg_length=len(message)
+            send_length=str(msg_length).encode(FORMAT)
+            send_length+=b' '*(HEADER - len(send_length))
+            client.send(send_length)
+            client.send(message)
+            f=client.recv(8192).decode(FORMAT)
+            if f=="User signed up successfully":
+                print("Signed up!")
+                if os.path.isdir("userprofs"):
+                    os.chdir("userprofs")
+                    with open(f"{s}.prof", "w") as w:
+                        w.write("")
+                else:
+                    os.mkdir("userprofs")
+                    os.chdir("userprofs")
+                    with open(f"{s}.prof", "w") as w:
+                        w.write("")
+                send(f"user-{s}")
+            elif f=="Successfully logged in!":
+                loggedin=True
+                s=s[0]
+                send(f"user-{s}")
+            elif f=="Non-existent":
+                quit()
+            elif f=="Incorrect password":
+                quit()
+            else:
+                print(f)
+                return f
+    return send(msg=msg)
--- main.py
+++ main.py
@@ -1,78 +1,33 @@
 import flet as ft
+import client
 
 def main(page: ft.Page):
-    page.fonts={
-        "Kanit": "https://raw.githubusercontent.com/google/fonts/master/ofl/kanit/Kanit-Bold.ttf"
-    }
-    page.theme = ft.Theme(font_family="Kanit")
-    firstPage=ft.Container(
-        content=ft.Column(
-            controls=[
-                ft.Row(alignment="spaceBetween",
-                    controls=[
-                        ft.Container(
-                            content=ft.Icon(
-                                ft.icons.MENU)),
-                        ft.Row(
-                            controls=[
-                                ft.Icon(ft.icons.SEARCH),
-                                ft.Icon(ft.icons.NOTIFICATIONS_OUTLINED)
-                            ]
-                        )
-                    ]
-                ),
-                ft.Container(height=20),
-                ft.Text(
-                    value="HELLO!",
-                    size=20
-                ),
-                ft.Button(
-                    text="Test",
-                    icon="park_rounded",
-                    icon_color="green400",
-                    on_click=lambda x: print("Hello :)")
-                ),
-                ft.IconButton(
-                    icon=ft.Icons.PAUSE_CIRCLE_FILLED_ROUNDED,
-                    icon_color="blue400",
-                    icon_size=20,
-                    tooltip="Pause record",
-                    on_click=lambda x: print("Paused!")
-                )
-            ]
-        )
-    )
-    page1=ft.Container()
-    page2=ft.Row(
-        controls=[
-            ft.Container(
-                width=400,
-                height=850,
-                bgcolor="#334a91",
-                border_radius=20,
-                padding=ft.padding.only(
-                    top=50,left=20, right=20, bottom=5
-                ),
-                content=ft.Column(
-                    controls=[
-                        firstPage
-                    ]
-                )
-            )
-        ]
-    )
-    container=ft.Container(
-        width=400,
-        height=850,
-        bgcolor="#000000",
-        border_radius=15,
-        content=ft.Stack(
-            controls=[
-                page1,
-                page2
-            ]
-        )
-        )
-    page.add(ft.SafeArea(container))
+    page.window.width=800
+    page.window.height=600
+    page.add(ft.Text(value="Hello, Winner"))
+    f=ft.Text(value="")
+    page.add(f)
+    def printText(f):
+        f.value=str(test.value)
+        page.update()
+    def changeWidth(e):
+        page.window.width=int(e)
+        page.update()
+    def getInven(l):
+        client.main("!connect")
+        log=client.main("user-Winner")
+        s=client.main("show-inven")
+        f.value=str(s)
+        x=client.main("!disconnect")
+        page.update()
+    test=ft.TextField(hint_text="Hi?")
+    rw=ft.Row(controls=[
+        ft.FloatingActionButton(text="500", on_click=lambda x: changeWidth(500)),
+        ft.FloatingActionButton(text="600", on_click=lambda x: changeWidth(600)),
+        ft.FloatingActionButton(text="Print", on_click=printText),
+        ft.FloatingActionButton(text="Test", on_click=getInven)
+    ])
+    page.add(test, rw)
+
 
 ft.app(main)
